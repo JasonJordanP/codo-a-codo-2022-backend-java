4a92bfa51c3436d1bf811530ceaf9c88b8c021b2
Jordan Perez <jordanperezurrutia@gmail.com>
2022-07-03 04:31:20 -0300

    add script sql for mysql and postgres

--- src/main/java/ar/com/codoacodo/connection/AdministradordeConexiones.java
+++ src/main/java/ar/com/codoacodo/connection/AdministradordeConexiones.java
@@ -6,11 +6,17 @@ import java.sql.DriverManager;
 public class AdministradordeConexiones {
 
 	public static Connection getConnection() {
-		String url= "jdbc:mysql://127.0.0.1:3306/codo-a-codo?serverTimeZone=UTC&userSSL=false";
+		/*String url= "jdbc:mysql://127.0.0.1:3306/codo-a-codo?serverTimeZone=UTC&userSSL=false";
 		String username = "root";
 		String password = "root";
 		
 		String driverName = "com.mysql.cj.jdbc.Driver"; //mysql|postgres
+		*/
+		
+		String url = System.getenv("DATASOURCE_URL");
+		String user = System.getenv("DATASOURCE_USERNAME");
+		String password = System.getenv("DATASOURCE_PASSWORD");
+		String driverName = System.getenv("DATASOURCE_DRIVER");
 		
 		//control de errores
 		
